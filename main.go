bd161140696695bdf93d649912cf8b4791cb0cc9
guipeng <aceguipeng@gmail.com>
2020-04-27 15:12:34 +0800

    增加日志设置

--- main.go
+++ main.go
@@ -29,6 +29,10 @@ var (
 )
 
 func main() {
+	//设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
+	logrus.SetOutput(os.Stdout)
+	//设置最低loglevel
+	logrus.SetLevel(logrus.InfoLevel)
 	app := cli.NewApp()
 	app.Name = appName
 	app.Usage = appUsage
